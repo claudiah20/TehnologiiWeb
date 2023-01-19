67c0968eb13b19ad30e3950994cf45f3b9aab995
Claudia <humeniucclaudia20@stud.ase.ro>
2023-01-19 02:43:19 +0200

    added final Frontend

--- Frontend/my_application/src/App.js
+++ Frontend/my_application/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import _default from 'react-overlays/cjs/Modal';
 import './App.css';
 
 function App() {
@@ -28,15 +29,76 @@ function App() {
 
     const handleAddEvent = () => {
 
-        fetch("http://localhost:7000/contacte", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ...contact
-            })
-        }).then((res) => res.json()).then((res) => setAdaugat(!adaugat));
+        let flag = false;
+
+        if ((contact.Nume.length < 3)) {
+            flag = false;
+            alert("Numele nu este valid!Numele trebuie sa contina minim 3 caractere");
+            window.location.reload();
+        }
+        else {
+            flag = true;
+        }
+        if ((contact.Prenume.length < 3)) {
+            alert("Prenumele nu este valid!Prenumele trebuie sa contina minim 3 caractere");
+            flag = false;
+            window.location.reload();
+        }
+        else {
+            flag = true;
+        }
+        if ((isNaN(Number(contact.Numar_Telefon))) && (contact.Numar_Telefon.length != 10)) {
+            flag = false;
+            console.log(isNaN(Number(contact.Numar_Telefon)) + "===" + (contact.Numar_Telefon.length != 10));
+            alert("Numarul de telefon trebuie sa fie de tip number!");
+            window.location.reload();
+        }
+        else {
+            flag = true;
+        }
+        if (!(isNaN(Number(contact.Numar_Telefon))) && (contact.Numar_Telefon.length != 10)) {
+            flag = false;
+            alert("Numarul de telefon trebuie sa contina exact 10 caractere!");
+            window.location.reload();
+        }
+        else {
+            flag = true;
+        }
+        if (contact.Mail.length <= 10) {
+
+            flag = false;
+            alert("Mailul nu este valid!Mailul trebuie sa contina minim 10 caractere ");
+            window.location.reload();
+        }
+        else {
+            flag = true;
+        }
+        if (!(contact.Mail.includes('@'))) {
+
+            flag = false;
+            alert("Mailul nu este valid!Mailul trebuie sa contina caracterul @ in text");
+            window.location.reload();
+        }
+        else {
+            flag = true;
+        }
+
+        console.log("flag: " + flag);
+
+        if (flag) {
+            fetch("http://localhost:7000/contacte", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    ...contact
+                })
+            }).then((res) => res.json()).then((res) => setAdaugat(!adaugat));
+        }
+        else {
+            alert("Datele nu au fost salvate intrucat nu sunt valide!")
+        }
     }
 
     const handleDeleteEvent = (ID) => {
@@ -53,13 +115,13 @@ function App() {
             </h1>
 
             <div className="form-container">
-                    <input class="input-top" type="text" placeholder="Nume" onChange={(evt) => handleInput(evt, "Nume")} />
-                    <input type="text" placeholder="Prenume" onChange={(evt) => handleInput(evt, "Prenume")} />
-                    <input type="text" placeholder="Numar Telefon" onChange={(evt) => handleInput(evt, "Numar_Telefon")} />
-                    <input type="text" placeholder="E-mail" onChange={(evt) => handleInput(evt, "Mail")} />
-                    <button onClick={() => {
-                        handleAddEvent();
-                    }}>Adauga Contact</button>
+                <input class="input-top" type="text" placeholder="Nume" onChange={(evt) => handleInput(evt, "Nume")} />
+                <input type="text" placeholder="Prenume" onChange={(evt) => handleInput(evt, "Prenume")} />
+                <input type="text" placeholder="Numar Telefon" onChange={(evt) => handleInput(evt, "Numar_Telefon")} />
+                <input type="text" placeholder="E-mail" onChange={(evt) => handleInput(evt, "Mail")} />
+                <button onClick={() => {
+                    handleAddEvent();
+                }}>Adauga Contact</button>
             </div>
             <table>
                 <thead>
@@ -76,20 +138,20 @@ function App() {
                     {
                         contacte.map((contact) => {
                             return (<tr>
-                                <td>{contact.ID}  
+                                <td>{contact.ID}
                                 </td>
                                 <td>{contact.Nume}</td>
                                 <td>{contact.Prenume}</td>
                                 <td>{contact.Numar_Telefon}</td>
                                 <td>{contact.Mail}</td>
-                                <td><button onClick={()=>{handleDeleteEvent(contact.ID)}}>Sterge</button></td>
+                                <td><button onClick={() => { handleDeleteEvent(contact.ID) }}>Sterge</button></td>
                             </tr>);
                         })
                     }
                 </tbody>
             </table>
         </div>
-        
+
     );
 }
 
